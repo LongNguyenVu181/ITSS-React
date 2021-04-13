7e8b2f35adc09246c465436e9f57a758c401a1cf
HSNT <hieu.jno1@gmail.com>
2021-04-13 21:25:06 +0700

    Add component App

--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
+import React, { useState} from 'react'
+import AddUserForm from './form/AddUserForm'
+import UpdateUser from './form/UpdateUser'
+import UserTable from './table/UsersTable'
 
-import './App.css';
+const App = () => {
+	const usersData = [
+		{ id: 1, name: 'Tania', username: 'floppydiskette' },
+		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
+		{ id: 3, name: 'Ben', username: 'benisphere' },
+	];
+	const initialFormState = { id: null, name: '', username: '' };
 
-import React, { useState, Fragment } from 'react'
-import UpdateForm from './form/UpdateUser'
-function App() {
-  const usersData = [
-    { id: 1, name: 'quanle', username: 'duyngadocton' },
-	]
-
-	const initialFormState = { id: null, name: '', username: '' }
-  const [ editing, setEditing ] = useState(true)
-  const [ users, setUsers ] = useState(usersData)
-	const [ currentUser, setCurrentUser ] = useState(users[0])
-  
-  const updateUser = (id, updatedUser) => {
-		setEditing(false)
-
-		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-	}
-	
-	
-  return (
-    <div className="App">
-     
-      <div className="content">
-        {editing ? 
-          <div className="Edit-form">
-            <h2>Edit user form</h2>
-            <UpdateForm
-              editing={editing}
-              setEditing ={setEditing}
-              currentUser={currentUser}
-              updateUser={updateUser}
-            />
-          </div>
-          :
-          <div className="Edit-form">
-            <h2>Edit user form</h2>
-            <UpdateForm
-              editing={editing}
-              setEditing ={setEditing}
-              currentUser={currentUser}
-              updateUser={updateUser}
-            />
-          </div>
-        }
-      </div>
-    </div>
-  );
+	const [ users, setUsers ] = useState(usersData);
+	const [ currentUser, setCurrentUser ] = useState(initialFormState);
+	const [ editing, setEditing ] = useState(false);
+	const addUser = user => {
+		user.id = users.length + 1;
+		setUsers([ ...users, user ])
+	};
+	const deleteUser = id => {
+		setEditing(false);
+		setUsers(users.filter(user => user.id !== id))
+	};
+	const updateUser = (id, updatedUser) => {
+		setEditing(false);
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+	};
+	const editRow = user => {
+		setEditing(true);
+		setCurrentUser({ id: user.id, name: user.name, username: user.username });
+	};
+	return (
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<>
+							<h2>Edit user</h2>
+							<UpdateUser
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</>
+					) : (
+						<>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</>
+					)}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
